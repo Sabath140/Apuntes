103757eb9e9399ce8201ebd2f750f96ae27563fe
Jose <sabathventas3@gmail.com>
2025-01-04 12:23:49 -0600

    Agregamos lo trabajado a la rama comenzando Don honesto

---
+++ js/index.js
@@ -0,0 +1,36 @@
+
+
+/*Este es un comentario
+de multiples lineas
+puede ser tan largo como yo quiera*/
+let nombre2 = "Jose";
+console.log(nombre2) 
+let edad = 19;
+console.log("Hola mi nombre es "+nombre2+" y mi edad es "+edad)
+console.log(`Hola mi nombre es ${nombre2} y mi edad es ${edad}`)
+console.log(edad)
+console.log(`19`)
+
+
+//Amigos es un tipo de dato array//
+let amigos = ["Ray","Yayo","Gerson",163];
+console.log(amigos[0])
+console.log(amigos[1])
+console.log(amigos[2])
+console.log(amigos[3])
+
+
+//Boleano V o F
+let verdadero = true;
+let falso = false;
+console.log(verdadero)
+console.log(falso)
+
+//Objeto
+let persona = {
+    nombre: "jose luis",
+    apellido: "sabath",
+    edad: 19,
+    nacionalidad: ["Mexicana", "Americana"]
+}
+console.log(persona)
